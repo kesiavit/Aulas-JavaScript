61ee3ea6bf308bb9450835a31cfc329a3d611729
Késia Vitória <k.gomeskesia@gmail.com>
2024-07-30 17:01:08 -0300

    variaveis

---
+++ aula4/scripts.js
@@ -0,0 +1,17 @@
+// Aprendendo a usar variáveis no JV
+
+//Declara uma variável sem valor
+
+var user
+
+console.log(user)
+
+//Declara uma variável com valor
+
+var email = "abc@emal.com"
+console.log(email)
+
+//Substituir o valor da variável
+
+email = "def@emal.com"
+console.log(email)
