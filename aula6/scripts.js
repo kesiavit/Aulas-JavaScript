fbf87071037a01b10b1ebdcaef827ccc264e41fc
Késia Vitória <k.gomeskesia@gmail.com>
2024-07-30 17:17:13 -0300

    let

---
+++ aula6/scripts.js
@@ -0,0 +1,14 @@
+// Let 
+
+//-> Declara uma variável sem valor
+
+let user 
+console.log("keke")
+
+//-> Declara uma variável com valor
+
+let email = "keke@gmail.com"
+console.log(email)
+
+email = "kel@gmail.com"
+console.log(email)
