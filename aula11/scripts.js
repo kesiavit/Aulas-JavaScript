2a57919e5358d3c122c82c8ab232d8dcd986a566
Késia Vitória <k.gomeskesia@gmail.com>
2024-07-31 21:43:37 -0300

    template literals

---
+++ aula11/scripts.js
@@ -0,0 +1,15 @@
+// Template literals (template strings, interpolação de strings)
+let username = "keke"
+let email = "kel@emal.com"
+
+// Passar mais de um parâmetro
+console.log(username, email)
+
+// Concatenar texto
+let message = "Olá," + username + "Você conectou com o e-mail" + email
+
+console.log(message)
+
+
+// Template literals
+console.log(`Olá, ${username}. Você conectou com o e-mail ${email}.`)
