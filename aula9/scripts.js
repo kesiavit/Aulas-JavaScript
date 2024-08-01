079708c290fa8d5610d58f7c9e498f699924714a
Késia Vitória <k.gomeskesia@gmail.com>
2024-07-31 21:15:32 -0300

    nomes de variaveis

---
+++ aula9/scripts.js
@@ -0,0 +1,30 @@
+// case-sensitive
+let username = "keke"
+let userName = "kel"
+
+console.log(username)
+console.log(userName)
+
+// Podemos utilizar caracteres e acentos especiais
+
+let _email = "abc@email.com"
+let Ação = "cadastrar" 
+//não é recomendado escrever em português e nem usar essas acentuações
+
+console.log(_email)
+console.log(cadastrar)
+
+// Não podemos começar uma variavel com um número
+// let 2user = "keke"
+
+// RECOMENDAÇÕES
+// escreva em inglês
+// não usar acentos e caracteres especiais
+// faça nomes descritivos
+
+//RECOMENDAÇÃO CamelCase
+//cameLo -> proxima palavra com letra maiúscula:
+let lastName = "Gomes"
+
+//RECOMENDAÇÃO snake_case (rastejar_____ como uma cobrinha)
+let product_name = "Teclado"
