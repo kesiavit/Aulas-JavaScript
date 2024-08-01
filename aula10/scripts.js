ff9b75650b704772ff190aaaa118b7d12c9e38f2
Késia Vitória <k.gomeskesia@gmail.com>
2024-07-31 21:31:28 -0300

    string

---
+++ aula10/scripts.js
@@ -0,0 +1,9 @@
+// String
+let username = "keke"
+console.log(username)
+console.log(typeof username)
+
+console.log(`
+    Uma string com acento grave permite
+    escrever múltiplas linhas.    
+`)
