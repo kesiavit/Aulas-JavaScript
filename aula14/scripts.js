060e54f8dc627c962306c0a1d24d99a9195251fd
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-02 13:14:40 -0300

    undefined and null

---
+++ aula14/scripts.js
@@ -0,0 +1,11 @@
+// undefined )indefinido e null
+
+let emptiness
+console.log("o valor é: ", emptiness)
+
+// undefined -> quando ainda não se tem um valor em uma variável
+
+let empty =null
+console.log("O valor é: ", empty)
+
+// null -> você não coloca nada na variável de propósito
