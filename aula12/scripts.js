909c047e39c877d5b2eb935aa4a0fb78535365c7
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-02 13:07:32 -0300

    number

---
+++ aula12/scripts.js
@@ -0,0 +1,17 @@
+// Number
+
+//typeof -> indica o tipo do conteudo exibido, mostrando essa informação lá no console
+
+console.log(typeof 7)
+
+//Inteiro positivo
+console.log(4)
+
+//Inteiro negativo
+console.log(-6)
+
+//Números reais ou float
+console.log(334.32)
+
+//Nan - not a Number
+console.log(3 / "keke")
