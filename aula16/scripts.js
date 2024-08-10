dabf1a335855276986135b9f387b9641bac87c30
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-10 14:10:22 -0300

    operadores aritiméticos

---
+++ aula16/scripts.js
@@ -0,0 +1,14 @@
+// Operadores aritiméticos
+
+console.log("Soma: ", 12 + 8)  //20
+console.log("concatenação: ", "12" + "8") //128
+
+console.log("Subtração: ", 12 - 10)
+
+console.log("Multiplicação: ", 3 * 5.5)
+
+console.log("divisão: ", 12 / 2)
+
+console.log("Resto da divisão: ", 13 % 2)
+
+console.log("exponencial: ", 3 ** 3)
