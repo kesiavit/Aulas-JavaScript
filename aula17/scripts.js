a2c512499a28e43e6dce39a386e97140bf192ec8
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 10:33:02 -0300

    incremento e decremento

---
+++ aula17/scripts.js
@@ -0,0 +1,28 @@
+let number = 10
+
+// INCREMENTO
+// number = number + 1 // 11
+
+// ou
+
+number++ //11
+
+// Incremento após (por isso não mostra o console)
+console.log(number++)
+console.log(number)
+
+// Incrementando antes
+console.log(++number)
+
+// Decremento (tirar)
+// console.log("Decremento após: ", number--)
+// console.log(number--)
+console.log("Decremento antes:", --number)
+
+// Adicionar/incrementar mais de um
+number += 10
+console.log(number)
+
+// Retirar/decrementar mais de um
+number -= 2
+console.log(number)
