7805dd3450ea7bde348d250854a94d93ec5e1b08
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:39:14 -0300

    operadores lógicos

---
+++ aula23/scripts.js
@@ -0,0 +1,15 @@
+let email = true
+let password = true
+
+// AND (E) && -> retorna como verdadeiro se todas as informções forem verdadeiras
+
+console.log(email && password) //true
+
+// OR (OU) || -> se um for verdadeiro já suficiente
+
+// let password = false
+// console.log(email || password) //true
+
+// NOT (negação) !
+console.log(!password) //false
+
