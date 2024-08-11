225ba25ea8849d453838881dd3e7e67e99f117a8
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:29:01 -0300

    maior, menor e igual a

---
+++ aula21/scripts.js
@@ -0,0 +1,16 @@
+let balance = 500
+let payment = 120
+
+// > maior que
+console.log(balance > payment) //true
+
+// < menor que
+console.log(balance < payment) //false
+
+// >= maior ou igual a
+balance = 120
+console.log(balance >= payment) //true
+
+// <= menor ou igual a
+balance = 500
+console.log(balance <= payment) //false
