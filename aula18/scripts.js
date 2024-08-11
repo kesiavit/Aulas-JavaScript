ed928bc0e505442f07d505f3f3678e8314072aba
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:01:20 -0300

    grouping operator

---
+++ aula18/scripts.js
@@ -0,0 +1,10 @@
+//Grouping Operator (ordem de precedência)
+
+let total = 2 + 3 * 4
+console.log(total)
+
+let total2 = (2 + 3) * 4
+console.log(total2)
+
+let average = (9.5 + 7 + 5) / 3
+console.log(average)
