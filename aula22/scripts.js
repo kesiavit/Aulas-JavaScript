82dbbfe198b2286901a3607222036f34ac465536
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:33:07 -0300

    operadores de atribuição

---
+++ aula22/scripts.js
@@ -0,0 +1,27 @@
+let value 
+
+// operador atribuição
+value = 1
+console.log(value)
+
+// incremento
+value += 2
+console.log(value)
+
+//decremento
+value -= 2
+console.log(value)
+
+// multiplicação
+value *= 3
+console.log(value)
+
+//divisão
+value /=2
+console.log(value)
+
+// resto da divisão
+value %= 2
+
+// potência
+value **= 2
