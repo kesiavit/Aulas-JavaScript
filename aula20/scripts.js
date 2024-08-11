468e6d6b0a44a22091d2fc26cb9542ed33f0b41a
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:23:19 -0300

    estritamente igual e diferente

---
+++ aula20/scripts.js
@@ -0,0 +1,14 @@
+// === estritamente igual a (olha o tipo e o valor)
+
+let one = 1
+let two = 2
+
+console.log (one === 1) //true
+console.log (one === "1") //false
+
+// !== estritamente diferente de
+
+console.log(one !== two) //true
+console.log(one !== 1) //false
+console.log(two !==2) //false
+console.log(two !== "2") //true
