d3b57004665e8f65a85fbd73a0422e2b2fc92bc5
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-11 11:10:01 -0300

    igual a e diferente de

---
+++ aula19/scripts.js
@@ -0,0 +1,13 @@
+let one = 1
+let two = 2
+
+// == igual a -> comparações
+
+console.log(one == two) //false
+console.log(one == 1) //true
+console.log(one == "1") //true -> olha para o conteúdo e não para o tipo
+
+// != diferente de
+console.log(one != two) //true
+console.log(one != 1) //false
+console.log(one != "1") //false
