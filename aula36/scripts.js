6d859b4c44e2cc849728a804228979305f709822
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-12 17:29:32 -0300

    Função Anônima

---
+++ aula36/scripts.js
@@ -0,0 +1,13 @@
+// Função anônima (não possui nome)
+
+const showMessage1 = function(){
+    return "Hello"
+}
+
+console.log(showMessage1())
+
+const showMessage2 = function(name){
+    return "Hi, " + name
+}
+
+console.log(showMessage2("Keke"))
