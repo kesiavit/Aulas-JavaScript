da6997327796ec3f37a2fc2d829d2b9b2fe0d221
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-12 16:10:58 -0300

    Switch case

---
+++ aula29/scripts.js
@@ -0,0 +1,17 @@
+// Switch
+
+let option = 2
+
+switch (option){
+    case 1:
+        console.log("Consultar pedido")
+        break
+    case 2:
+        console.log("Falar com atendente")
+        break
+    case 3:
+        console.log("cancelar pedido")
+        break
+    default: //caso não exista nenhum dos casos
+    console.log("Opção inválida")
+}
