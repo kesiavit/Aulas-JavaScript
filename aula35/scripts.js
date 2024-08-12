c4ecf9bcd06f6a3e8459e2a30ea9ba00af39d53a
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-12 17:23:57 -0300

    Comentário de documentação

---
+++ aula35/scripts.js
@@ -0,0 +1,19 @@
+/*
+    Comentário de documentação em JavaScript (sintaxe de JSDoc).
+    O ISDoc é um padrão para incorporar documentação no código-fonte a partir desses comentários.
+*/
+
+/**
+ * Authenticates the user
+ * 
+ * @param {string} email user email
+ * @param {string} password more than 6 characters
+ * @returns {Number} user id.
+ */
+function signIn(email, password){
+    // Fluxo de autentificação do usuário.
+
+    return 7
+}
+
+signIn("Keke@gmail.com, 8475248")
