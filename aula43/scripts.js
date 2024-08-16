89182964f5089618738134b18fa9e495b5e8d5f6
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-16 18:48:36 -0300

    Manipulando Atributos

---
+++ aula43/scripts.js
@@ -0,0 +1,9 @@
+// Manipulando Atributos
+
+const input = document.querySelector("input")
+
+// input.setAttribute("disabled", true) -> consegue deixar ele desabilitado, atualizando um atributo
+
+// input.setAttribute("type", "file") -> mudou o tipo da botão
+
+// input.removeAttribute("id") -> retira um atributo
