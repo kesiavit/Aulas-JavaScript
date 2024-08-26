bbf2b0bfcc7b265a20219a453cd9119057245be6
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-26 15:19:47 -0300

    Texto maiúsculo e minúsculo

---
+++ aula56/scripts.js
@@ -0,0 +1,12 @@
+// MÉTODOS DE TEXTO
+// Texto maiúsculo e minúsculo
+
+let message = "Estou estudando os fundamentos do JS"
+
+console.log(message)
+
+// Exibir o texto em maiúsculo
+console.log(message.toUpperCase())
+
+// Exibir o texto em minúsculo
+console.log(message.toLowerCase())
