becd8d3d700ad5ec19673a60ceeb16b8da0c134b
Késia Vitória <k.gomeskesia@gmail.com>
2024-08-28 15:10:12 -0300

    Aprendendo sobre arrays

---
+++ aula62/scripts.js
@@ -0,0 +1,12 @@
+/* Aprendendo sobre arrays
+
+Array é uma coleção ordenada de valores
+
+Podemos comparar uma array a uma lista, onde cada item da lista possui uma posição específica, conhecida como índice.
+
+Começa pelo 0.
+
+fruits = (maça, abacaxi, malancia, banana)
+console.log(fruits[2]) -> melancia
+
+*/
