80beedf8ff9f8836396bb7ed53d5ce43af184ed5
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-03 18:02:03 -0300

    Verificando se existe um conteúdo no array

---
+++ aula68/scripts.js
@@ -0,0 +1,8 @@
+// Verificando se existe um conteúdo no array
+
+let fruits = ["Apple", "Orange", "Banana"]
+
+// "includes" verifica se um item existe no Array
+console.log(fruits.includes("Orange")) //true
+
+console.log(fruits.includes("Strawberry")) //false
