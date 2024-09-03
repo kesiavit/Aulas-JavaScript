4c1b30ec459d538ccfe73df86ff10573624624cb
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-03 17:14:13 -0300

    Criando e acessando um array

---
+++ aula63/scripts.js
@@ -0,0 +1,14 @@
+// Criando e acessando um array
+
+// Criando o Array.
+let fruits = ['Apple', 'Banana', 'Orange']
+console.log(fruits)
+
+// Quantidade de itens do Array.
+console.log(fruits.length)
+
+// Acessa o item pelo índice.
+console.log(fruits[1])
+
+// Obtém dinamicamente o último item.
+console.log(fruits[fruits.length - 1])
