666b1af79935e0042249e7017a6c1fea11bb68a8
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-03 17:20:54 -0300

    Convertendo uma string para array

---
+++ aula64/scripts.js
@@ -0,0 +1,10 @@
+// Convertendo uma string para array
+
+let userName = "Késia Vitória"
+console.log(userName)
+
+// Cria um Array com os nomes separando pelo espaço.
+console.log(userName.split(" "))
+
+// Cria um Array com as letras.
+console.log(Array.from(userName))
