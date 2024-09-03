4be7cb65d2e5f4f37e89c0bc7b3117dc9b9351fa
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-03 17:32:10 -0300

    Adicionando e removendo um item do array

---
+++ aula65/scripts.js
@@ -0,0 +1,19 @@
+// Adicionando e removendo um item do array
+
+let users = []
+
+// O "push" adiciona um item ao final do Array.
+users.push("Keke")
+users.push("Kel")
+users.push("Késia")
+
+//O "unshift", faz o texto ir como primeiro na lista
+users.unshift("Vitória") 
+
+// Remove do início do Array.
+users.shift()
+
+// Remove do final do Array
+users.pop()
+
+console.log(users)
