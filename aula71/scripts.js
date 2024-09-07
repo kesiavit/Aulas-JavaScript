cdac4217b520429d6350633fc5c1bfbf3d22e162
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-07 17:23:43 -0300

    Loop do while

---
+++ aula71/scripts.js
@@ -0,0 +1,11 @@
+// Loop do while -> repete até que a condição especificada seja VERDADEIRA. No entento, a instrução será executada pelo menos uma vez antes da condição ser verificada.
+
+let value = 3
+
+do {
+    value++
+    console.log(value)
+
+} while (value < 10)
+
+console.log("Segue fluxo...")
