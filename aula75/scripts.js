0a65debad9597a145e2ecf0f6b1d66ce8616148e
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-14 21:47:20 -0300

    Break

---
+++ aula75/scripts.js
@@ -0,0 +1,32 @@
+/* Break encerra a execução da repetição ou switch para seguir para a instrução seguinte.
+*/
+
+let option = 3
+
+switch(option){
+    case 1:
+        console.log("Cadastrar")
+        break
+
+    case 2:
+        console.log("Cadastrar")
+        break
+
+    case 3:
+            console.log("Cadastrar")
+            break
+
+    default:
+        console.log("Opção inválida")
+        break
+}
+
+
+// Utilizando o break para finalizar a repetição.
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break
+    }
+
+    console.log(i)
+}
