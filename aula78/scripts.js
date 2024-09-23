89966daeb29aa37f8222b8abb21de39cf9468e76
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-23 13:58:24 -0300

    Definindo uma data e uma hora específica

---
+++ aula78/scripts.js
@@ -0,0 +1,12 @@
+// Definindo uma data e uma hora específica
+
+// Define com ano, mês (0-11), dia.
+console.log(new Date(2024, 8, 23)) 
+
+// Define data e hora (ano, mês, dia, hora, minuto e segundo)
+console.log(new Date(2024, 8, 23, 13, 54, 0))
+
+// Definindo data e hora com string.
+console.log(new Date("2024-09-23T13:55:00"))
+
+console.log(new Date("Sep 23, 2024 13:57:00"))
