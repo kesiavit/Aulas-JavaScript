35490fa3b3fc86156a300fc86c3a5ba5a9f818f9
Késia Vitória <k.gomeskesia@gmail.com>
2024-09-23 14:05:50 -0300

    Modificando uma data e uma hora

---
+++ aula79/scripts.js
@@ -0,0 +1,23 @@
+// Modificando uma data e uma hora
+
+let date = new Date("Sep 23, 2024 14:00:00")
+
+// Modificar o ano.
+date.setFullYear(2040)
+
+// Modificar o mês (começa com 0)
+date.setMonth(7)
+
+// Modificar o dia
+date.setDate(10)
+
+// Modificar a hora
+date.setHours(20)
+
+// Modificar o minuto
+date.setMinutes(12)
+
+// Modificar os segundos
+date.setSeconds(20)
+
+console.log(date)
