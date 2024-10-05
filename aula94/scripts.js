da4ced975e74a829184ff2d3e193ef2bffe61de8
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 17:27:54 -0300

    Criando um objeto prototype

---
+++ aula94/scripts.js
@@ -0,0 +1,15 @@
+// Criando um objeto prototype
+
+const address = {
+    city: "Jacaraú",
+    country: "Brasil"
+}
+
+console.log(address)
+
+const users = ["Keke", "Nanda", "Milena"]
+
+console.log(users)
+
+const userName = "Késia Vitória"
+console.log(userName.__proto__)
