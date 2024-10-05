dbf3edda392d7b1701d4c0f0ea0c20ea4f5fe541
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 10:26:30 -0300

    Fuso horário

---
+++ aula84/scripts.js
@@ -0,0 +1,14 @@
+/*
+FUSO HORÁRIO
+
+IIMESTAMP => Uma data representada como um número (carimbo de data/hora)
+
+É um valor que representa um ponto específico no tempo
+
+- A data de referência do JS: baseado no valor de tempo em milissegundos desde a meia noite de 01 de janeiro de 1970, UTC.
+
+
+DUAS FORMAS DE INTERPRETAR DATA E HORA:
+- Local (determinado pelo ambiente que está executando - dispositivo do usuário)
+- Tempo Universal Coordenado (UTC)
+*/
