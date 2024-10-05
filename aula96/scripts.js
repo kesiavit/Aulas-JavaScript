26e6aadcef25017bea134f97e18af923bc9191c6
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 17:52:30 -0300

    Utilizando classes para criar erros customizados

---
+++ aula96/scripts.js
@@ -0,0 +1,19 @@
+// Como utilizar classes para criar erros customizados
+
+class MyCustomError {
+    constructor(message){
+        this.message = "CLASSE DE ERRO CUSTOMIZADA: " + message
+    }
+}
+
+try {
+    throw new Error("Erro genérico")
+
+    throw new MyCustomError("Erro personalizado lançado!")
+} catch (error) {
+    if(error instanceof MyCustomError){
+        console.log(error.message)
+    } else{
+        console.log("Não foi possível executar!")
+    }
+}
