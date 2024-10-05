4a8517470af06dfd8d26cc2b0ceef2ad037a3129
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 11:00:54 -0300

    Intl

--- aula84/scripts.js
+++ aula84/scripts.js
@@ -12,3 +12,30 @@ DUAS FORMAS DE INTERPRETAR DATA E HORA:
 - Local (determinado pelo ambiente que está executando - dispositivo do usuário)
 - Tempo Universal Coordenado (UTC)
 */
+
+
+
+// Intl (usada também para manipular data e hora) é a API de Internacionalização do ECMAScript.
+
+
+// Obtém informações da localidade.
+const currentLocale = Intl.DateTimeFormat().resolvedOptions()
+
+console.log(currentLocale)
+
+
+
+// Exibe no formato de acordo com a localidade
+
+console.log(new Intl.DateTimeFormat("pt-BR").format(new Date())) // Passou a data e hora atual (new date()), e mandou formatar usando o padrão ("pt-BR")
+
+console.log(new Intl.DateTimeFormat("en-US").format(new Date()))
+
+
+const date = new Date()
+
+// Obtém a diferença em minutos do timezone.
+console.log(date.getTimezoneOffset())
+
+// Obém a diferença em horas do timezone.
+console.log(date.getTimezoneOffset() / 60)
