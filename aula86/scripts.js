86c264a733a3b365087289d359a4e3ca170f66e3
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 11:37:10 -0300

    Classes

---
+++ aula86/scripts.js
@@ -0,0 +1,18 @@
+/*
+CLASSES
+
+- São uma forma de criar objetos e definir seu comportamento por meio de costrutores e métodos.
+
+- Fornecem uma sintaxe mais agradável para a criação de objetos e herança de protótipos (syntax sugar)
+
+
+CONSTRUTORES E MÉTODOS
+
+- Classe: modelo para criar objetos;
+- Construtor: método especial chamado quando um objeto é instanciado a partir da classe;
+- Métodos: funções associadas a objetos e descrevem o comportamento desses objetos.
+
+
+HERANÇA
+- Uma classe pode herdar propriedades e métodos de outra classe, permitindo a reutilização de código.
+*/
