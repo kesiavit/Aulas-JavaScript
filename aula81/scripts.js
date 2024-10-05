d88a267914f8d4cb8afe8a15cd039767c6a8f61d
Késia Vitória <k.gomeskesia@gmail.com>
2024-10-05 08:50:57 -0300

    Convertendo uma data para string

---
+++ aula81/scripts.js
@@ -0,0 +1,12 @@
+// Convertendo uma data para string
+
+let date = new Date("2024-07-02T14:30:00")
+
+// Converte para String.
+console.log(date.toString())
+
+// Retorna somente a data.
+console.log(date.toDateString())
+
+// Retorna somente a hora.
+console.log(date.toTimeString())
